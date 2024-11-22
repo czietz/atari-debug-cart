3b65914e9b335518ecf459089bdfc6f2124c53bb
Christian Zietz <czietz@gmx.net>
2024-11-22 12:46:57 +0100

    License under GPL 3

--- debug.c
+++ debug.c
@@ -1,8 +1,23 @@
-/* debug-cart: Repurpose Sidecartridge as debug output by encoding the
+/*
+ * debug-cart: Repurpose Sidecartridge as debug output by encoding the
  * output data on the Atari address bus (on A8-A1).
  *
- * (C) 2024 by Christian Zietz <czietz@gmx.net>.
- * 
+ *
+ * Copyright (c) 2024 Christian Zietz <czietz@gmx.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
 
 #include <stdint.h>
